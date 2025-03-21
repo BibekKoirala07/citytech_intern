2a36c111b1419ce4f548d5bc3e44aabd0c87a92e
BibekKoirala07 <www.koiralabibek2058@gmail.com>
2025-03-21 16:41:03 +0545

    docs done !!!

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const countInversion = (arr) => {
+const args = process.argv.slice(2);
+
+let array;
+if (args.length === 0) {
+  array = [7, 6, 5, 4, 3, 2, 1];
+  console.log("No array provided. Using default array:" + array);
+} else {
+  array = args.map(Number);
+  console.log("the given array is " + array);
+}
+
+const countInversion = (array) => {
+  let count = 0;
   for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.length; j++) {
-      if (arr[i] > arr[j]) {
+      if (array[i] > array[j]) {
         count++;
       }
     }
   }
+  return count;
 };
 
-const totalInversionOfArray = countInversion([1, 2, 3, 4]);
+const totalInversionOfArray = countInversion(array);
+console.log("The total Inversion of Array is " + totalInversionOfArray);
